bae403dac2d58f2634690316228c728cdf91270d
Ivan <chakin.2001@mail.ru>
2022-03-31 19:54:30 +0300

    + Modal.vue + Teleport

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -9,25 +9,39 @@
   </div>
   <div v-else>
     Content
+    <button @click="handleModalShow" class="btn btnPrimary">Open modal</button>
+    <Modal v-if="showModal" @onClose="handleModalShow">
+      example modal
+    </Modal>
   </div>
 </template>
 
 <script>
 import {ref} from 'vue'
 import Spinner from '@/components/UI/Spinner.vue'
+import Modal from '@/components/UI/Modal.vue'
 
 export default {
   components: {
-    Spinner
+    Spinner,
+    Modal
   },
   setup() {
-    const isLoading = ref(true)
+    const isLoading = ref(false)
 
-    setTimeout(() => {
-      isLoading.value = false
-    }, 2000)
 
-    return { isLoading }
+
+
+    const showModal = ref(false)
+    const handleModalShow = () => {
+      const nextShowModal = showModal.value = !showModal.value
+      showModal.value = nextShowModal
+    }
+    // setTimeout(() => {
+    //   isLoading.value = false
+    // }, 2000)
+
+    return { isLoading, showModal, handleModalShow }
   }
 }
 </script>
